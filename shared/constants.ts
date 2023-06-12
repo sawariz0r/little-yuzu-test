2970af5801835f364a4789632cd849a86e2d8706
Oscar Nilsson <oscar@osuka.dev>
2023-06-12 03:17:34 +0200

    Added constants

---
+++ shared/constants.ts
@@ -0,0 +1,47 @@
+export const EMAIL_REGEX = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+export const PHONE_REGEX =
+  /^(\+46|0)([\d]{2,3}[-\s]?[\d]{2,3}[-\s]?[\d]{2,3}[-\s]?[\d]{2,3})$/;
+export const MOCK_MENU = [
+  {
+    name: "Greek Salad",
+    price: 12.99,
+    description:
+      "Our delicious salad is served with Feta cheese and peeled cucumber. Includes tomatoes, onions, olives, salt and oregano in the ingredients.",
+    image: "greekSalad.jpg",
+    category: "starters",
+  },
+  {
+    name: "Bruschetta",
+    price: 7.99,
+    description:
+      "Delicious grilled bread rubbed with garlic and topped with olive oil and salt. Our Bruschetta includes tomato and cheese.",
+    image: "bruschetta.jpg",
+    category: "starters",
+  },
+  {
+    name: "Grilled Fish",
+    price: 20.0,
+    description: "Fantastic grilled fish seasoned with salt.",
+    image: "grilledFish.jpg",
+    category: "mains",
+  },
+  {
+    name: "Pasta",
+    price: 6.99,
+    description: "Delicious pasta for your delight.",
+    image: "pasta.jpg",
+    category: "mains",
+  },
+  {
+    name: "Lemon Dessert",
+    price: 4.99,
+    description: "You can't go wrong with this delicious lemon dessert!",
+    image: "lemonDessert.jpg",
+    category: "desserts",
+  },
+];
+export const MOCK_CATEGORIES = [
+  { name: "starters", label: "Starters" },
+  { name: "mains", label: "Mains" },
+  { name: "desserts", label: "Desserts" },
+];
